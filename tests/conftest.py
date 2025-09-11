d0b75ec1e11eb9f6e877af6f3cfb8949322b6359
Alexander-Adjei <akadjei017@st.ug.edu.gh>
2025-09-11 16:34:05 +0000

    Priject folder created successfully

---
+++ tests/conftest.py
@@ -0,0 +1,21 @@
+# class solution:
+#     def generateParenthesis(self, n: int) -> list[str]:
+#         ans, sol = [], []
+
+#         def backtracking(openn, close):
+#             if len(sol) == 2*n:
+#                 ans.append(''.join(sol))
+#                 return
+            
+#             if openn < n:
+#                 sol.append('(')
+#                 backtracking(openn+1, close)
+#                 sol.pop()
+
+#             if openn > close:
+#                 sol.append(')')
+#                 backtracking(openn, close+1)
+#                 sol.pop()
+            
+#         backtracking(0, 0)
+#         return ans
